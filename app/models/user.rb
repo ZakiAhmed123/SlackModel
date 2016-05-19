3f6aba2472adbe579e090cb56378ab9b0d4a17ec
“Zaki <reach.zaki.ahmed@gmail.com>
2016-05-19 13:07:17 -0500

    fixed comma error

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ActiveRecord::Base
 
   has_secure_password
 
-  validates :user_name, :password, :first_name, :last_name presence: true
+  validates :user_name, :password, :first_name, :last_name, presence: true
   validates :user_name, :email, uniqueness: true
   attachment :photo
 
